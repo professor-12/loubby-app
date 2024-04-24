5769a65dc3689a35fe8fd4b2638061ccc9e85c11
Badejo Emmanuel <badejoemmanuel805@gmail.com>
2024-04-24 19:28:46 +0100

    Implemet authentication in the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
-const page = () => {
-    return <div>page</div>;
+const Page = () => {
+    const router = useRouter();
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        const user = localStorage.getItem("user");
+        if (!token || !user) return router.push("onboarding/login");
+
+        return router.push("/dashboard");
+    }, [router]);
+
+    return null;
 };
 
-export default page;
+export default Page;
