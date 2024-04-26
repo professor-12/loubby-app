265f439ef9d8e7961e320a1c0ac487f20024b7e4
Badejo Emmanuel <badejoemmanuel805@gmail.com>
2024-04-26 20:13:26 +0100

    Create ui-design for top_nav_bar in dashboard_page

---
+++ src/components/Search.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Image from "next/image";
+
+const Search = ({ className }: { className: string }) => {
+    return (
+        <div className="flex bg-[#F9FAFB] overflow-hidden border-slate-300 border focus:border-blue-400  group rounded-lg space-x-2 lg:min-w-[50%] px-3">
+            <Image
+                src={"/searchIcon.svg"}
+                className="bg-transparent"
+                alt=""
+                width={18}
+                height={18}
+            />
+            <input
+                placeholder="Search"
+                type="text"
+                className="p-2 text-sm placeholder-text-sm bg-transparent focus:group-[]:ring-blue-600 focus:outline-none w-full"
+            />
+        </div>
+    );
+};
+
+export default Search;
