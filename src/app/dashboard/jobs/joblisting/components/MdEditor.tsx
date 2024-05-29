557040ad985e3cb27ee91417f9d6d9e6ee9799b1
Badejo Emmanuel <badejoemmanuel805@gmail.com>
2024-05-29 20:11:39 +0100

    Fix active route bug

--- src/app/dashboard/jobs/joblisting/components/MdEditor.tsx
+++ src/app/dashboard/jobs/joblisting/components/MdEditor.tsx
@@ -1,22 +1,51 @@
 "use client";
-import React, { useState } from "react";
-import FroalaEditor from "react-froala-wysiwyg";
-const MarkdownEditor = () => {
-    const [markdown, setMarkdown] = useState("Hey");
+// src/Tiptap.jsx
+import {
+    useEditor,
+    EditorContent,
+    FloatingMenu,
+    BubbleMenu,
+} from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import ToolBar from "./ToolBar";
+import { useEffect } from "react";
 
-    const handleChange = (value: any) => {
-        setMarkdown(value);
+// define your extension array
+
+const extensions = [StarterKit];
+
+const content = "<p>Hello World!</p>";
+
+const Tiptap = () => {
+    const editor = useEditor({
+        extensions,
+        content,
+        editorProps: {
+            attributes: {
+                class: "flex flex-col px-4 py-3 justify-start border-b border-r border-1 border-gray-300",
+            },
+        },
+    });
+
+    const d = () => {
+        const selection = getSelection() as any;
+        const highlightedText = selection?.toString();
+
+        if (highlightedText) {
+            console.log("Highlighted Text:", highlightedText);
+
+            // Retrieve current highlights from localStorage
+        } else {
+            console.log("No text highlighted.");
+        }
     };
 
     return (
-        <div className="text-sm">
-            <FroalaEditor config={{
-                toolbarButtons: {
-                    button: ["bold"]
-                }
-            }} />
-        </div>
+        <>
+            <ToolBar />
+            <div className="id" onClick={d}>Hey booman</div>
+        </>
     );
 };
 
-export default MarkdownEditor;
+export default Tiptap;
