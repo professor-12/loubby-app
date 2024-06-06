44538b8a503cfbb019f5706fdd9725d4162ba1d3
Badejo Emmanuel <badejoemmanuel805@gmail.com>
2024-06-06 19:01:10 +0100

    Fix a ui problem

--- src/app/dashboard/components/TopNav/HeaderButton.tsx
+++ src/app/dashboard/components/TopNav/HeaderButton.tsx
@@ -16,10 +16,23 @@ const HeaderButton = () => {
             className="relative"
         >
             <div
-                className="bg-blue-600 h-8 w-8 rounded-lg text-center flex items-center justify-center text-white"
+                className="bg-blue-600 h-7 w-7 md:w-8 md:h-8 rounded-lg text-center flex items-center justify-center text-white"
                 onClick={(_) => _}
             >
-                +
+                <svg
+                    stroke="currentColor"
+                    fill="currentColor"
+                    stroke-width="0"
+                    viewBox="0 0 16 16"
+                    id="addDrop"
+                    color="#fff"
+                    height="15"
+                    width="15"
+                    xmlns="http://www.w3.org/2000/svg"
+                    style={{color: "rgb(255, 255, 255);"}}
+                >
+                    <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"></path>
+                </svg>
             </div>
             <div onClick={(e) => e.stopPropagation()}>
                 {openModal && (
