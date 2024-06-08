10862f176ee30629d36d06c352339e49f59e560b
Badejo Emmanuel <badejoemmanuel805@gmail.com>
2024-06-08 22:11:34 +0100

    Fix background image position and size in dashboard header Component

--- src/app/dashboard/components/Header.tsx
+++ src/app/dashboard/components/Header.tsx
@@ -8,8 +8,8 @@ const Header = () => {
     }, []);
 
     return (
-        <div className="bg-[url('/d.png')] flex  bg-no-repeat bg-[-79rem]  min-h-[11rem]  overflow-hidden rounded-lg">
-            <div className="bg-blue-500 w-full flex justify-between flex-col bg-opacity-65 p-6">
+        <div className="bg-[url('/d.png')] header flex  bg-no-repeat bg-[-79rem]  min-h-[11rem]  overflow-hidden rounded-lg">
+            <div className="bg-blue-600/50 w-full flex justify-between flex-col bg-opacity-65 p-6">
                 <div className="">
                     <h1 className="md:text-xl font-medium text-white">
                         Good Evening {user?.last_name} !
@@ -18,11 +18,11 @@ const Header = () => {
                         Welcome to your remote talent hiring dashboard
                     </p>
                 </div>
-                <div className="flex flex-wrap md:flex-nowrap  gap-4 justify-between lg:justify-start items-center">
+                <div className="flex flex-wrap md:flex-nowrap  gap-5 justify-between lg:justify-start items-center">
                     <div className="w-full md:w-auto">
-                    <button className="bg-white text-nowrap w-full p-[0.5rem] text-sm font-medium px-3 rounded-lg">
-                        Hire Talents
-                    </button>
+                        <button className="bg-white text-nowrap w-full p-[0.5rem] text-sm font-medium px-3 rounded-lg">
+                            Hire Talents
+                        </button>
                     </div>
                     <div className="flex md:justify-start gap-3 justify-between px-6 md:px-0 w-full items-center">
                         <p className="text-white text-xs md:text-sm underline">
--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const loading = () => {
     return (
         <div className="min-h-screen flex items-center justify-center">
-            <div className="text-4xl underline underline-offset-4 font-medium animate-pulse duration-75">
+            <div className="text-4xl underline underline-offset-4 font-medium animate-bounce duration-75">
                 Loading
             </div>
             <span className="text-4xl">...</span>
