b017bbeff3fdbe6e83919e3d6bc1c9edc446bbc4
Professor <135185597+professor-12@users.noreply.github.com>
2024-06-16 17:19:51 +0100

    Update NavLink.tsx

--- src/app/dashboard/components/NavLink.tsx
+++ src/app/dashboard/components/NavLink.tsx
@@ -1,27 +1,34 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname , useRouter } from "next/navigation";
 import React from "react";
 
 const NavLink = ({
     link,
     svg,
     name,
+    setOpen
 }: {
     link: string;
     svg: string;
     name?: string;
+    setOpen: any;
 }) => {
     const pathName = usePathname();
+    const router = useRouter()
     const active =
         (pathName.split("/")[2] || "dashboard") ===
         (link.split("/")[2]?.split("?")[0] || "dashboard");
     return (
         <li className={`hover:bg-active  rounded ${active && "bg-active"}`}>
             <div className="p-3 px-5">
-                <Link
-                    href={link ?? ""}
+                <div
+                    onClick={()=> {
+                        router.push(link ?? "")
+                        setOpen(false)
+                    }}
+                    
                     className={` rounded-md flex space-x-4 items-center`}
                 >
                     <span className="">
@@ -34,7 +41,7 @@ const NavLink = ({
                     >
                         {name}
                     </p>
-                </Link>
+                </div>
             </div>
         </li>
     );
